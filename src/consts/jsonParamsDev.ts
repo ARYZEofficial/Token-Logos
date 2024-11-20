7f6d5a1f483fffd8c795917d79c96a8ba88514be
Dima Rud <dima.rud1995@gmail.com>
2024-11-20 14:09:25 +0200

    up patch

--- src/consts/jsonParamsDev.ts
+++ src/consts/jsonParamsDev.ts
@@ -3,10 +3,9 @@ import {IJson} from '../interfaces/IJson';
 export const JSON_DEV_PARAMS: IJson = {
   name: 'ARYZE Token Palace Test',
   timestamp: '2024-11-20T10:34:43.594Z',
-  version: {major: 1, minor: 1, patch: 10},
+  version: {major: 1, minor: 1, patch: 15},
   tags: {},
-  logoURI:
-    'https://raw.githubusercontent.com/ARYZEofficial/Token-Logos/main/MAMA-logo.png',
+  logoURI: 'https://raw.githubusercontent.com/ARYZEofficial/Token-Logos/main/MAMA-logo.png',
   keywords: ['ARYZE', 'Stablecoins', 'eUSD', 'eGOLD'],
-  tokens: [],
+  tokens: []
 };
