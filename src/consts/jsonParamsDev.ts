6684399622816ae7267d9f5aa0aa222e9a0a69c8
Dima Rud <dima.rud1995@gmail.com>
2024-11-20 14:18:31 +0200

    up

--- src/consts/jsonParamsDev.ts
+++ src/consts/jsonParamsDev.ts
@@ -2,10 +2,11 @@ import {IJson} from '../interfaces/IJson';
 
 export const JSON_DEV_PARAMS: IJson = {
   name: 'ARYZE Token Palace Test',
-  timestamp: '2024-11-20T10:51:08.071Z',
-  version: {major: 1, minor: 1, patch: 15},
+  timestamp: '2024-11-20T12:18:22.279Z',
+  version: {major: 1, minor: 1, patch: 16},
   tags: {},
-  logoURI: 'https://raw.githubusercontent.com/ARYZEofficial/Token-Logos/main/MAMA-logo.png',
+  logoURI:
+    'https://raw.githubusercontent.com/ARYZEofficial/Token-Logos/main/MAMA-logo.png',
   keywords: ['ARYZE', 'Stablecoins', 'eUSD', 'eGOLD'],
-  tokens: []
+  tokens: [],
 };
--- src/consts/jsonParamsProd.ts
+++ src/consts/jsonParamsProd.ts
@@ -2,10 +2,11 @@ import {IJson} from '../interfaces/IJson';
 
 export const JSON_PROD_PARAMS: IJson = {
   name: 'ARYZE Token Palace',
-  timestamp: '2024-11-20T10:51:08.072Z',
-  version: {major: 2, minor: 0, patch: 37},
+  timestamp: '2024-11-20T12:18:22.280Z',
+  version: {major: 2, minor: 0, patch: 38},
   tags: {},
-  logoURI: 'https://raw.githubusercontent.com/ARYZEofficial/Token-Logos/main/MAMA-logo.png',
+  logoURI:
+    'https://raw.githubusercontent.com/ARYZEofficial/Token-Logos/main/MAMA-logo.png',
   keywords: ['ARYZE', 'Stablecoins', 'eUSD', 'eGOLD'],
-  tokens: []
+  tokens: [],
 };
