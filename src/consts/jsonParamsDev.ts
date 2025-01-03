1364d1994deedf04a2be4bfead728a0a14bcfaeb
Dima Rud <dima.rud1995@gmail.com>
2025-01-03 12:10:49 +0200

    new solum logos

--- src/consts/jsonParamsDev.ts
+++ src/consts/jsonParamsDev.ts
@@ -2,8 +2,8 @@ import {IJson} from '../interfaces/IJson';
 
 export const JSON_DEV_PARAMS: IJson = {
   name: 'ARYZE Token Palace Test',
-  timestamp: '2024-12-28T12:10:30.031Z',
-  version: {major: 1, minor: 1, patch: 28},
+  timestamp: '2025-01-03T10:10:26.729Z',
+  version: {major: 1, minor: 1, patch: 29},
   tags: {},
   logoURI:
     'https://raw.githubusercontent.com/ARYZEofficial/Token-Logos/main/MAMA-logo.png',
