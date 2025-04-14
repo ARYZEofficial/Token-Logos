333e1eee8176f538ffbd6cdc5ba840a94b82b030
ivan lysakov <ivl@aryze.io>
2025-04-14 17:58:41 +0300

    add LEMX on Binance. fix2

--- src/consts/jsonParamsDev.ts
+++ src/consts/jsonParamsDev.ts
@@ -2,8 +2,8 @@ import {IJson} from '../interfaces/IJson';
 
 export const JSON_DEV_PARAMS: IJson = {
   name: 'ARYZE Token Palace Test',
-  timestamp: '2025-04-14T14:57:16.097Z',
-  version: {major: 1, minor: 1, patch: 56},
+  timestamp: '2025-04-14T14:58:26.994Z',
+  version: {major: 1, minor: 1, patch: 57},
   tags: {},
   logoURI:
     'https://raw.githubusercontent.com/ARYZEofficial/Token-Logos/main/MAMA-logo.png',
--- src/consts/jsonParamsProd.ts
+++ src/consts/jsonParamsProd.ts
@@ -2,8 +2,8 @@ import {IJson} from '../interfaces/IJson';
 
 export const JSON_PROD_PARAMS: IJson = {
   name: 'ARYZE Token Palace',
-  timestamp: '2025-04-14T14:57:16.099Z',
-  version: {major: 2, minor: 0, patch: 78},
+  timestamp: '2025-04-14T14:58:26.992Z',
+  version: {major: 2, minor: 0, patch: 79},
   tags: {},
   logoURI:
     'https://raw.githubusercontent.com/ARYZEofficial/Token-Logos/main/MAMA-logo.png',
