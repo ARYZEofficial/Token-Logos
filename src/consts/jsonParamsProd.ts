073ae33af72707f8e980ec9528f3f66cbafa4370
Dima Rud <dima.rud1995@gmail.com>
2025-04-16 15:20:48 +0300

    add all 9 nfts

--- src/consts/jsonParamsProd.ts
+++ src/consts/jsonParamsProd.ts
@@ -2,8 +2,8 @@ import {IJson} from '../interfaces/IJson';
 
 export const JSON_PROD_PARAMS: IJson = {
   name: 'ARYZE Token Palace',
-  timestamp: '2025-04-16T11:21:47.122Z',
-  version: {major: 2, minor: 0, patch: 92},
+  timestamp: '2025-04-16T12:20:49.472Z',
+  version: {major: 2, minor: 0, patch: 93},
   tags: {},
   logoURI:
     'https://raw.githubusercontent.com/ARYZEofficial/Token-Logos/main/MAMA-logo.png',
