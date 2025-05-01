04d005cccc3a63e1b5e7c6fc154fc9445fd3a071
Dima Rud <dima.rud1995@gmail.com>
2025-05-01 18:12:53 +0300

    add doppelgangers

--- src/consts/jsonParamsDev.ts
+++ src/consts/jsonParamsDev.ts
@@ -2,8 +2,8 @@ import {IJson} from '../interfaces/IJson';
 
 export const JSON_DEV_PARAMS: IJson = {
   name: 'ARYZE Token Palace Test',
-  timestamp: '2025-05-01T15:03:24.095Z',
-  version: {major: 1, minor: 1, patch: 88},
+  timestamp: '2025-05-01T15:12:53.778Z',
+  version: {major: 1, minor: 1, patch: 90},
   tags: {},
   logoURI:
     'https://raw.githubusercontent.com/ARYZEofficial/Token-Logos/main/MAMA-logo.png',
