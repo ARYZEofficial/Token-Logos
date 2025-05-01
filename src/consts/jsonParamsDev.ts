06001eefbfb63668e45f9d73ae55fdb7ab6e9600
Dima Rud <dima.rud1995@gmail.com>
2025-05-01 14:26:21 +0300

    fix lmed erc20 logo

--- src/consts/jsonParamsDev.ts
+++ src/consts/jsonParamsDev.ts
@@ -2,8 +2,8 @@ import {IJson} from '../interfaces/IJson';
 
 export const JSON_DEV_PARAMS: IJson = {
   name: 'ARYZE Token Palace Test',
-  timestamp: '2025-05-01T11:23:49.468Z',
-  version: {major: 1, minor: 1, patch: 84},
+  timestamp: '2025-05-01T11:26:22.294Z',
+  version: {major: 1, minor: 1, patch: 86},
   tags: {},
   logoURI:
     'https://raw.githubusercontent.com/ARYZEofficial/Token-Logos/main/MAMA-logo.png',
