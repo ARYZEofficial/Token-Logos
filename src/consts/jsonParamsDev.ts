a323156c3e8fe77394361bee6592edf0f3ce960e
Dima Rud <dima.rud1995@gmail.com>
2025-05-02 15:34:47 +0300

    add usdc

--- src/consts/jsonParamsDev.ts
+++ src/consts/jsonParamsDev.ts
@@ -2,8 +2,8 @@ import {IJson} from '../interfaces/IJson';
 
 export const JSON_DEV_PARAMS: IJson = {
   name: 'ARYZE Token Palace Test',
-  timestamp: '2025-05-01T15:12:53.778Z',
-  version: {major: 1, minor: 1, patch: 90},
+  timestamp: '2025-05-02T12:34:47.994Z',
+  version: {major: 1, minor: 1, patch: 92},
   tags: {},
   logoURI:
     'https://raw.githubusercontent.com/ARYZEofficial/Token-Logos/main/MAMA-logo.png',
--- src/consts/jsonParamsProd.ts
+++ src/consts/jsonParamsProd.ts
@@ -2,8 +2,8 @@ import {IJson} from '../interfaces/IJson';
 
 export const JSON_PROD_PARAMS: IJson = {
   name: 'ARYZE Token Palace',
-  timestamp: '2025-05-01T15:12:53.780Z',
-  version: {major: 2, minor: 0, patch: 112},
+  timestamp: '2025-05-02T12:34:47.995Z',
+  version: {major: 2, minor: 0, patch: 114},
   tags: {},
   logoURI:
     'https://raw.githubusercontent.com/ARYZEofficial/Token-Logos/main/MAMA-logo.png',
