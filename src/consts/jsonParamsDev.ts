378cd194388eff0beefe3cb87869e865afa96c50
Dima Rud <dima.rud1995@gmail.com>
2025-05-02 16:48:36 +0300

    add w to erc20 lemon names

--- src/consts/jsonParamsDev.ts
+++ src/consts/jsonParamsDev.ts
@@ -2,8 +2,8 @@ import {IJson} from '../interfaces/IJson';
 
 export const JSON_DEV_PARAMS: IJson = {
   name: 'ARYZE Token Palace Test',
-  timestamp: '2025-05-02T12:34:47.994Z',
-  version: {major: 1, minor: 1, patch: 92},
+  timestamp: '2025-05-02T13:48:36.821Z',
+  version: {major: 1, minor: 1, patch: 94},
   tags: {},
   logoURI:
     'https://raw.githubusercontent.com/ARYZEofficial/Token-Logos/main/MAMA-logo.png',
--- src/consts/jsonParamsProd.ts
+++ src/consts/jsonParamsProd.ts
@@ -2,8 +2,8 @@ import {IJson} from '../interfaces/IJson';
 
 export const JSON_PROD_PARAMS: IJson = {
   name: 'ARYZE Token Palace',
-  timestamp: '2025-05-02T12:34:47.995Z',
-  version: {major: 2, minor: 0, patch: 114},
+  timestamp: '2025-05-02T13:48:36.823Z',
+  version: {major: 2, minor: 0, patch: 116},
   tags: {},
   logoURI:
     'https://raw.githubusercontent.com/ARYZEofficial/Token-Logos/main/MAMA-logo.png',
