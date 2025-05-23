41b0aa4a2f64c2f9689f233e35b9a9a318534f94
Dima Rud <dima.rud1995@gmail.com>
2025-05-23 12:34:08 +0300

    fix wLEMX

--- src/consts/jsonParamsProd.ts
+++ src/consts/jsonParamsProd.ts
@@ -2,8 +2,8 @@ import {IJson} from '../interfaces/IJson';
 
 export const JSON_PROD_PARAMS: IJson = {
   name: 'ARYZE Token Palace',
-  timestamp: '2025-05-20T14:22:04.859Z',
-  version: {major: 2, minor: 0, patch: 118},
+  timestamp: '2025-05-23T09:34:09.033Z',
+  version: {major: 2, minor: 0, patch: 120},
   tags: {},
   logoURI:
     'https://raw.githubusercontent.com/ARYZEofficial/Token-Logos/main/MAMA-logo.png',
