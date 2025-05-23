2465e507e25a5ce34f6cda80b80b7d8efd81d458
Dima Rud <dima.rud1995@gmail.com>
2025-05-23 16:52:42 +0300

    test new repo git

--- src/consts/jsonParamsProd.ts
+++ src/consts/jsonParamsProd.ts
@@ -2,8 +2,8 @@ import {IJson} from '../interfaces/IJson';
 
 export const JSON_PROD_PARAMS: IJson = {
   name: 'ARYZE Token Palace',
-  timestamp: '2025-05-23T13:49:27.341Z',
-  version: {major: 2, minor: 0, patch: 122},
+  timestamp: '2025-05-23T13:52:42.954Z',
+  version: {major: 2, minor: 0, patch: 124},
   tags: {},
   logoURI:
     'https://raw.githubusercontent.com/ARYZEofficial/Token-Logos/main/MAMA-logo.png',
