2510443edea663fc95ea57439a5aa2da1301a9ee
Dima Rud <dima.rud1995@gmail.com>
2025-06-06 15:49:11 +0300

    add ryopay

--- src/consts/jsonParamsDev.ts
+++ src/consts/jsonParamsDev.ts
@@ -2,8 +2,8 @@ import {IJson} from '../interfaces/IJson';
 
 export const JSON_DEV_PARAMS: IJson = {
   name: 'ARYZE Token Palace Test',
-  timestamp: '2025-05-30T10:34:34.698Z',
-  version: {major: 1, minor: 1, patch: 104},
+  timestamp: '2025-06-06T12:49:12.123Z',
+  version: {major: 1, minor: 1, patch: 106},
   tags: {},
   logoURI:
     'https://raw.githubusercontent.com/ARYZEofficial/Token-Logos/main/MAMA-logo.png',
--- src/consts/jsonParamsProd.ts
+++ src/consts/jsonParamsProd.ts
@@ -2,8 +2,8 @@ import {IJson} from '../interfaces/IJson';
 
 export const JSON_PROD_PARAMS: IJson = {
   name: 'ARYZE Token Palace',
-  timestamp: '2025-05-30T10:34:34.700Z',
-  version: {major: 2, minor: 0, patch: 126},
+  timestamp: '2025-06-06T12:49:12.125Z',
+  version: {major: 2, minor: 0, patch: 128},
   tags: {},
   logoURI:
     'https://raw.githubusercontent.com/ARYZEofficial/Token-Logos/main/MAMA-logo.png',
