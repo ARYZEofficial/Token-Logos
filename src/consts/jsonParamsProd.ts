580b51bd10d9cbc9214dd6b9f635c723eed47c99
Dima Rud <dima.rud1995@gmail.com>
2025-06-06 18:06:30 +0300

    add blox

--- src/consts/jsonParamsProd.ts
+++ src/consts/jsonParamsProd.ts
@@ -2,8 +2,8 @@ import {IJson} from '../interfaces/IJson';
 
 export const JSON_PROD_PARAMS: IJson = {
   name: 'ARYZE Token Palace',
-  timestamp: '2025-06-06T12:49:12.125Z',
-  version: {major: 2, minor: 0, patch: 128},
+  timestamp: '2025-06-06T15:06:31.092Z',
+  version: {major: 2, minor: 0, patch: 130},
   tags: {},
   logoURI:
     'https://raw.githubusercontent.com/ARYZEofficial/Token-Logos/main/MAMA-logo.png',
