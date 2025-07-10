e8df18d02c979ab4c6bdcfd421444ffca7a80ed3
Dima Rud <dima.rud1995@gmail.com>
2025-07-10 13:04:13 +0300

    name lemx2

--- src/consts/jsonParamsDev.ts
+++ src/consts/jsonParamsDev.ts
@@ -2,8 +2,8 @@ import {IJson} from '../interfaces/IJson';
 
 export const JSON_DEV_PARAMS: IJson = {
   name: 'ARYZE Token Palace Test',
-  timestamp: '2025-07-07T11:46:12.258Z',
-  version: {major: 1, minor: 1, patch: 119},
+  timestamp: '2025-07-10T10:04:13.976Z',
+  version: {major: 1, minor: 1, patch: 120},
   tags: {},
   logoURI:
     'https://raw.githubusercontent.com/ARYZEofficial/Token-Logos/main/MAMA-logo.png',
