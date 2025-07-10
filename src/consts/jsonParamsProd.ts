5053af86a817708c7d59752a6568f020aa9afab2
Dima Rud <dima.rud1995@gmail.com>
2025-07-10 14:49:35 +0300

    whizgrid-stage

--- src/consts/jsonParamsProd.ts
+++ src/consts/jsonParamsProd.ts
@@ -2,8 +2,8 @@ import {IJson} from '../interfaces/IJson';
 
 export const JSON_PROD_PARAMS: IJson = {
   name: 'ARYZE Token Palace',
-  timestamp: '2025-07-10T10:04:13.977Z',
-  version: {major: 2, minor: 0, patch: 142},
+  timestamp: '2025-07-10T11:49:35.991Z',
+  version: {major: 2, minor: 0, patch: 143},
   tags: {},
   logoURI:
     'https://raw.githubusercontent.com/ARYZEofficial/Token-Logos/main/MAMA-logo.png',
